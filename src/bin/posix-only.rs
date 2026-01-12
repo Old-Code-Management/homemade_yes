ab887201c1a5caa337483ba87c3ac2312dc7b8ef
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-01-12 13:01:15 -0500

    Update posix-only.rs

--- src/bin/posix-only.rs
+++ src/bin/posix-only.rs
@@ -1,8 +1,6 @@
 use std::env;
 use std::io::{self, Write};
 
-// Note: This version is not actually POSIX-compliant, and the filename is a misnomer. See README.md for more information.
-
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     let joined;
@@ -14,6 +12,6 @@ fn main() {
     } else {joined = args.join(" ") + "\n"; joined.as_bytes()};
 
     let mut io_handle = io::stdout().lock();
-    let buf = output_str.repeat(16384/output_str.len());
+    let buf = output_str.repeat(4096);
     loop {if io_handle.write_all(&buf).is_err() {return;}}
 }
